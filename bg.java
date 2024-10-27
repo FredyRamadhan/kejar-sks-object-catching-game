c72f5fec2c6d58d364b24bd0afb52120e9aa53bb
febrianernanda <febrianernanda13@gmail.com>
2024-10-27 18:25:26 +0700

    Update bg.java

--- bg.java
+++ bg.java
@@ -4,7 +4,7 @@ public class bg extends World
 {
     public bg()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        // Create a new world with 1280x720 cells with a cell size of 1x1 pixels.
         super(1280, 720, 1);
     }
 }
