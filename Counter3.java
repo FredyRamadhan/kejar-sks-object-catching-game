0575c60bce2f6a000eb49729506cde66d6b3ccd1
febrianernanda <febrianernanda13@gmail.com>
2024-10-27 18:01:13 +0700

    Update Counter3.java

--- Counter3.java
+++ Counter3.java
@@ -1,17 +1,7 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Counter3 here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Counter3 extends Actor
 {
-    /**
-     * Act - do whatever the Counter3 wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
     public void act()
     {
         
