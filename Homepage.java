64b29a27e748529b24f0c1795dffa5aa28a856e2
febrianernanda <febrianernanda13@gmail.com>
2024-10-27 18:05:25 +0700

    Update Homepage.java

--- Homepage.java
+++ Homepage.java
@@ -1,28 +1,13 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*;
 
-/**
- * Write a description of class Homepage here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
 public class Homepage extends World
 {
-
-    /**
-     * Constructor for objects of class Homepage.
-     * 
-     */
     public Homepage()
     {    
         super(1280, 720, 1);
         prepare();
     }
-
-    /**
-     * Prepare the world for the start of the program.
-     * That is: create the initial objects and add them to the world.
-     */
+    
     private void prepare()
     {
         play play = new play();
